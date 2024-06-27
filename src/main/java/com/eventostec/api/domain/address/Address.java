628d6c1815fd9bb8718d74ca37b5910c1c59423e
Thiago Dias <thiaguinhodias.15@gmail.com>
2024-06-27 01:56:05 -0300

    feat: create coupon entity

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,26 @@
+package com.eventostec.api.domain.address;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name="address")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
--- src/main/java/com/eventostec/api/domain/coupon/Coupon.java
+++ src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -1,4 +1,4 @@
-package com.eventostec.api.domain.cupom;
+package com.eventostec.api.domain.coupon;
 
 import com.eventostec.api.domain.event.Event;
 import jakarta.persistence.*;
