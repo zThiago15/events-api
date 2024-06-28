603336b09b83ead0a779ed355f5122fdec5f3b46
Thiago Dias <thiaguinhodias.15@gmail.com>
2024-06-28 12:45:58 -0300

    feat: add event controller post request

---
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -0,0 +1,34 @@
+package com.eventostec.api.controller;
+
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Event> create(
+            @RequestParam("title") String title,
+            @RequestParam(value="description", required = false) String description,
+            @RequestParam("date") Long date,
+            @RequestParam("city") String city,
+            @RequestParam("uf") String uf,
+            @RequestParam("remote") boolean remote,
+            @RequestParam("eventUrl") String eventUrl,
+            @RequestParam(value="image", required = false) MultipartFile image) {
+
+        EventRequestDTO eventRequestDTO = new EventRequestDTO(title, description, date, city, uf, remote, eventUrl, image);
+
+        Event newEvent = this.eventService.createEvent(eventRequestDTO);
+        return ResponseEntity.ok(newEvent);
+    }
+}
