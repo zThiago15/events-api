fcd2bff43d6eb2929b938b8c24a412b78bcb6af0
Thiago Dias <thiaguinhodias.15@gmail.com>
2024-06-29 02:53:55 -0300

    feat: coupon service creation

--- src/main/java/com/eventostec/api/controller/CouponController.java
+++ src/main/java/com/eventostec/api/controller/CouponController.java
@@ -4,23 +4,24 @@ import com.eventostec.api.domain.coupon.Coupon;
 import com.eventostec.api.domain.coupon.CouponRequestDTO;
 import com.eventostec.api.repositories.CouponRepository;
 import com.eventostec.api.repositories.EventRepository;
+import com.eventostec.api.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RestController
+@RequestMapping("/api/coupon")
 public class CouponController {
 
     @Autowired
     private CouponService couponService;
 
     @PostMapping("/event/{eventId}")
-    private ResponseEntity<Coupon> addCouponsToEvent(@PathVariable UUID eventId, @RequestBody CouponRequestDTO data) {
-        Coupon coupons = couponService.addCouponToEvent(eventId, data);
-        return ResponseEntity.ok(coupons);
+    private ResponseEntity<Coupon> addCouponToEvent(@PathVariable UUID eventId, @RequestBody CouponRequestDTO data) {
+        Coupon coupon = couponService.addCouponToEvent(eventId, data);
+        return ResponseEntity.ok(coupon);
 
     }
 }
---
+++ src/main/java/com/eventostec/api/service/CouponService.java
@@ -0,0 +1,35 @@
+package com.eventostec.api.service;
+
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.repositories.CouponRepository;
+import com.eventostec.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO couponData) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+        Coupon coupon = new Coupon();
+        coupon.setCode(couponData.code());
+        coupon.setDiscount(couponData.discount());
+        coupon.setValid(new Date(couponData.valid()));
+
+        return couponRepository.save(coupon);
+    }
+}
